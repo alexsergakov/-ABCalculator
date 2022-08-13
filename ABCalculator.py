146bcc4f4d7b96ebde43aa99f5b8f67c663a66ad
sergakov <as0312@mail.ru>
2022-08-13 22:45:47 +0500

    Изменение дизайна главного окна

--- ABCalculator.py
+++ ABCalculator.py
@@ -21,42 +21,51 @@ root.geometry("280x300")
 root.title("A/B калькулятор")
 
 # Добавление метки заголовка
-lblTitle = tk.Label(text = "A/B калькулятор", font = ('Helvetika', 16, 'bold'), fg = '#0000cc')
+lblTitle = tk.Label(text = "A", font = ('Helvetika', 16, 'bold'), fg = '#0066ff')
+lblTitle.place(x=25, y=5)
+
+lblTitle = tk.Label(text = "/", font = ('Helvetika', 25, 'bold'))
+lblTitle.place(x=43, y=10)
+
+lblTitle = tk.Label(text = "B", font = ('Helvetika', 16, 'bold'), fg = '#008800')
 lblTitle.place(x=55, y=20)
 
+lblTitle = tk.Label(text = "Калькулятор", font = ('Helvetika', 16, 'bold'), fg = '#0000cc')
+lblTitle.place(x=90, y=15)
+
 # Добавление метки заголовка контрольной группы
-lblTitle = tk.Label(text = "Контрольная группа", font = ('Helvetika', 12, 'bold'), fg = '#0066ff')
-lblTitle.place(x=25, y=55)
+lblTitle = tk.Label(text = "Контрольная группа", font = ('Helvetika', 12, 'bold'))
+lblTitle.place(x=60, y=50)
 
 # Добавление полей ввода контрольной группы
-lblVisitors1 = tk.Label(text = "Посетители:", font = ('Helvetika', 10, 'bold'))
-lblVisitors1.place(x=25, y=85)
+lblVisitors1 = tk.Label(text = "Посетители:", font = ('Helvetika', 10, 'bold'), fg = '#0066ff')
+lblVisitors1.place(x=25, y=80)
 
 entVisitors1 = tk.Entry(font = ('Helvetika', 10, 'bold'))
-entVisitors1.place(x=115, y=85, width=90, height=20)
+entVisitors1.place(x=160, y=80, width=90, height=20)
 
-lblVisitors1 = tk.Label(text = "Конверсии:", font = ('Helvetika', 10, 'bold'))
-lblVisitors1.place(x=25, y=115)
+lblVisitors1 = tk.Label(text = "Конверсии:", font = ('Helvetika', 10, 'bold'), fg = '#0066ff')
+lblVisitors1.place(x=25, y=110)
 
 entVisitors1 = tk.Entry(font = ('Helvetika', 10, 'bold'))
-entVisitors1.place(x=115, y=115, width=90, height=20)
+entVisitors1.place(x=160, y=110, width=90, height=20)
 
 # Добавление метки заголовка тестовой группы
-lblTitle = tk.Label(text = "Тестовая группа", font = ('Helvetika', 12, 'bold'), fg = '#008800')
-lblTitle.place(x=25, y=145)
+lblTitle = tk.Label(text = "Тестовая группа", font = ('Helvetika', 12, 'bold'))
+lblTitle.place(x=75, y=150)
 
 # Добавление полей ввода тестовой группы
-lblVisitors1 = tk.Label(text = "Посетители:", font = ('Helvetika', 10, 'bold'))
-lblVisitors1.place(x=25, y=175)
+lblVisitors1 = tk.Label(text = "Посетители:", font = ('Helvetika', 10, 'bold'), fg = '#008800')
+lblVisitors1.place(x=25, y=180)
 
 entVisitors1 = tk.Entry(font = ('Helvetika', 10, 'bold'))
-entVisitors1.place(x=115, y=175, width=90, height=20)
+entVisitors1.place(x=160, y=180, width=90, height=20)
 
-lblVisitors1 = tk.Label(text = "Конверсии:", font = ('Helvetika', 10, 'bold'))
-lblVisitors1.place(x=25, y=205)
+lblVisitors1 = tk.Label(text = "Конверсии:", font = ('Helvetika', 10, 'bold'), fg = '#008800')
+lblVisitors1.place(x=25, y=210)
 
 entVisitors1 = tk.Entry(font = ('Helvetika', 10, 'bold'))
-entVisitors1.place(x=115, y=205, width=90, height=20)
+entVisitors1.place(x=160, y=210, width=90, height=20)
 
 # Добавление кнопки "Расчитать"
 btnProcess = tk.Button(root, text="Расчитать", font = ('Helvetika', 10, 'bold'), command=popup_window)
